ff0b57c568ba1e45eee799cc60ba49a77aa03b09
Julius Ondari <juliusomwanza@gmail.com>
2017-06-27 11:33:42 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<link href="assets/images/favicon-128x128-1.png" rel="shortcut icon" type="image/x-icon">
+	<meta content="Easily and Instantly compare, book and schedule freight forwarding and transport services online. Check best prices, transit time and feedback, immediate confirmation 24/7. Cargo-n-Trucks is a fair & dependable platform leveraging on technology and an extensive network of Logistic and Transport professionals to provide instant access to truck space." name="description">
+	<title>Cargo&ndash;&&ndash;Trucks: Efficient & Cost Effective Logistics and Trucking &ndash; An Airland Global trucking service</title>
+	<link href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" rel="stylesheet"><!--[if lte IE 8]>
+        <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-old-ie-min.css">
+    <![endif]-->
+	<!--[if gt IE 8]><!-->
+	<link href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-min.css" rel="stylesheet"><!--<![endif]-->
+	<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<style>
+	@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+	</style><!--[if lte IE 8]>
+            <link rel="stylesheet" href="css/layouts/marketing-old-ie.css">
+        <![endif]-->
+	<!--[if gt IE 8]><!-->
+	<link href="css/layouts/marketing.css" rel="stylesheet"><!--<![endif]-->
+
+	<style>
+  #myBtn {
+	position: fixed;
+	top: 50%;
+	right: 0px;
+	z-index: 99;
+	border: none;
+	outline: none;
+	background-color: red;
+	color: white;
+	cursor: pointer;
+	padding: 1.2rem;
+	border-radius: 0;
+}
+#myBtn:hover {
+	background-color: #555;
+}
+	</style>
+	<script src="https://cdn.firebase.com/libs/firebaseui/2.1.1/firebaseui.js">
+	</script>
+	<link href="https://cdn.firebase.com/libs/firebaseui/2.1.1/firebaseui.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<!-- Google Analytics -->
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	         ga('create', 'UA-79272671-1', 'auto');
+	         ga('send', 'pageview');
+	</script> <!-- /Google Analytics -->
+	<button id="myBtn" title="Instant booking">Book a delivery</button>
+	<div class="header">
+		<div class="menu">
+			<?php include 'includes/menu.php';?>
+		</div>
+	</div>
+	<div class="splash-container">
+		<div class="splash">
+			<h1 class="splash-head">Cargo & Trucks</h1>
+			<h2>An Airland Global Service</h2>
+			<p class="splash-subhead">The easiest way to book a truck and move goods whenever and wherever.</p>
+			<p class="inline-buttons"><a class="pure-button tooltip">Need Transport Services?<span class="tooltiptext">Tooltip text</span></a> <a class="pure-button" href="/">Need to Post a Truck?</a></p>
+		</div>
+	</div>
+	<div class="content-wrapper">
+		<div class="content">
+			<h2 class="content-head is-center">On-Demand Online Truck Booking</h2>
+			<hr width="20%">
+			<div class="center">
+				<div>
+					<p>Cargo & trucks offers easy and instant access to an unlimited number of reliable and safe trucks. With one click, get instant access to cargo trucks while saving up to 30% of the transportation fees.</p>
+				</div>
+			</div>
+			<div class="pure-g">
+				<div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-3">
+					<h3 class="content-subhead"><a href="#"><i class="fa fa-truck"></i> Full Container Transport</a></h3>
+					<p align="justify"><a href="#"><b>In need of a truck?</b></a> Cargo n Trucks provides safe, instant access to <a href="#">trucks for hire</a>. Whether your load requires a <a href="#">10-wheller</a> for light <a href="#">20-ft containers</a> or a <a href="#">40 ft truck</a>, we’ve got you covered for the short or long haul.</p>
+				</div>
+				<div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-3">
+					<h3 class="content-subhead"><a href="#"><i class="fa fa-mobile"></i> Loose Cargo-(Break Bulk)</a></h3>
+					<p align="justify">Most times you will need to transport cargo as is. Loose Cargo is transported as <a href="#">units</a>, in/or <a href="#">bags</a>, <a href="#">boxes</a>, <a href="#">crates</a>, <a href="#">drums</a>, <a href="#">barrels</a> or <a href="#">pallets</a>. We provide <a href="#">pick-up trucks</a>, <a href="#">Vannetes</a>, <a href="#">flat-beds</a>, <a href="#">High-side</a>, <a href="#">low-beds</a>, and <a href="#">tipper-trucks</a>.</p>
+				</div>
+				<div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-3">
+					<h3 class="content-subhead"><a href="#"><i class="fa fa-th-large"></i> Office and Home Moving</a></h3>
+					<p align="justify"><a href="#">Moving a business</a> or <a href="#">moving a house</a> should not be a hassle. Get access to <a href="#">cheapest rates instantly</a> and <a href="#">schedule a move</a> in the shortest time possible conveniently.</p>
+				</div><!--<div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4 ">
+                <h3 class="content-subhead">
+                    <a href= "#"><i class="fa fa-shopping-cart"></i>
+                    Instant Package Delivery</a>
+                </h3>
+                <p align="justify">
+                   Send and receive packages locally within 90 minutes,
+                   or choose a 1-hour time slot at a time convenient for you. We offer fast and cost-efficient <a href="#">e-commerce  fulfilment</a> services for on-line sellers and buyers.
+                </p>
+            </div> -->
+			</div>
+		</div>
+		<div class="ribbon l-box-lrg pure-g">
+			<div class="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5"><img alt="File Icons" class="pure-img-responsive" src="img/common/full-truck-load.png" width="300"></div>
+			<div class="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
+				<h2 class="content-head content-head-ribbon">BECOME OUR PARTNER.</h2>
+				<p>Transportation Logistics by its very nature depends on relationships and connections to ensure success. By becoming <a class="white-text" href="#">our partner</a>, you will enjoy free instant access to all <a class="white-text" href="#">available cargo</a> and faster processing of payment. We are an efficient extension of your marketing, sales and dispatch team. We take care of all the details, from order acquisition to volume estimation, leaving you open to focus on your core business. There couldn’t be an easier way to save time and money.</p>
+				<p>Partner with us and take part in the rapidly growing market for instant booking, same-day or timed delivery!</p>
+				<div>
+					<a class="pure-button button-xlarge center"><i class="fa fa-cog"></i> Drive with Us</a>
+				</div>
+			</div>
+		</div>
+		<div class="content">
+			<h2 class="content-head is-center">RELIABLE PARTNER FOR YOUR MOVE</h2>
+			<div class="pure-g">
+				<div class="l-box-lrg pure-u-1 pure-u-md-2-5">
+					<h4>Request a Quick Quote</h4>
+					<form class="pure-form pure-form-stacked">
+						<fieldset>
+							<input id="name" placeholder="Your Name" type="text"> <input id="email" placeholder="Your Email" type="email">
+							<textarea class="pure-input-1" placeholder="Please provide any additional information regarding your Quick Quote"></textarea> <button class="pure-button center" type="submit">Submit Query</button>
+						</fieldset>
+					</form>
+				</div>
+				<div class="l-box-lrg pure-u-1 pure-u-md-3-5">
+					<h4>Why Book a truck with Us</h4>
+					<p>Our motivation is the commitment to deliver our promise; No excuses. We invest in our people, <a href="#">technology</a> and our relationships to ensure thet we provide accurate capacity for our clients and deliver cargo safely, on time and on budget.</p>
+					<h4>Performance explained.</h4>
+					<p>Our Technology is reflective and is designed to ensure complete visibility of transactions and moves. This is done to enable accurate matching of client cargo and truck capacity at any moment of the system.</p>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<?php include 'includes/footer.php';?>
+		</div>
+	</div>
+	<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js">
+	</script>
+	<script>
+	     // Initialize Firebase
+	     var config = {
+	       apiKey: "AIzaSyBMZgN5Z92U_b1j2pXCFhQtLEn-ILKTnKY",
+	       authDomain: "cargontrucks.firebaseapp.com",
+	       databaseURL: "https://cargontrucks.firebaseio.com",
+	       projectId: "cargontrucks",
+	       storageBucket: "cargontrucks.appspot.com",
+	       messagingSenderId: "924513477417"
+	     };
+	     firebase.initializeApp(config);
+	</script>
+</body>
+</html>
