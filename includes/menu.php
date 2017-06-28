d98859ebda5f82414ea42b0683205afe4a894c37
Julius Ondari <juliusomwanza@gmail.com>
2017-06-28 13:49:08 +0300

    Add files via upload

---
+++ includes/menu.php
@@ -0,0 +1,26 @@
+<?php
+echo '<div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link ">Overview</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link ">Instant Booking</a></li>
+			<li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover"><a href="#" id="menuLink1" class="pure-menu-link">Solutions</a>
+				<ul class="pure-menu-children">
+					<li class="pure-menu-item"><a href="#" class="pure-menu-link">Container</a></li>
+					<li class="pure-menu-item"><a href="#" class="pure-menu-link">Loose Cargo</a></li>
+					<li class="pure-menu-item"><a href="#" class="pure-menu-link">Tanker Cargo</a></li>
+					<li class="pure-menu-item"><a href="#" class="pure-menu-link">Cargo Clearing</a></li>
+
+				</ul>
+			</li>
+            <li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover"><a href="#" id="menuLink1" class="pure-menu-link">Get Access</a>
+				<ul class="pure-menu-children">
+					<li class="pure-menu-item"><a href="#" class="pure-menu-link">Shippers</a></li>
+					<li class="pure-menu-item"><a href="#" class="pure-menu-link">Partner</a></li>
+					<li class="pure-menu-item"><a href="#" class="pure-menu-link">Manager</a></li>
+					<li class="pure-menu-item"><a href="#" class="pure-menu-link">Agents</a></li>
+				</ul>
+			</li>
+        </ul>
+    </div>';
+?>
